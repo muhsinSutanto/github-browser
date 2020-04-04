dd6236746cf7f32208056691db0ac817bc8978c8
muhsin sutanto <muhsin@femaledaily.com>
2020-04-05 00:51:12 +0700

    add spinner

--- src/component/user/Users.js
+++ src/component/user/Users.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import UserItem from './UserItem'
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
-class Users extends Component {
-  
-  render() { 
-    return ( 
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
       <div style={userStyle}>
-        {this.props.users.map(user => {
-          return(
-           <UserItem key={user.id} user={user} />
-          )
+        {users.map((user) => {
+          return <UserItem key={user.id} user={user} />;
         })}
       </div>
-     );
+    );
   }
-}
+};
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
-}
- 
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
+
 export default Users;
